f3434f4bd7339e177e07a305488ef8bdf4c9d7e0
jinju <jinju6288@naver.com>
2021-07-25 15:44:37 +0900

    fixed untracked files

--- Study_hotfive/src/com/workbook/ch1_IO와사칙연산/n10171고양이/Main.java
+++ Study_hotfive/src/com/workbook/ch1_IO와사칙연산/n10171고양이/Main.java
@@ -3,8 +3,16 @@ package com.workbook.ch1_IO와사칙연산.n10171고양이;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(" \\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+/*
+ \    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
 	}
 
 }
