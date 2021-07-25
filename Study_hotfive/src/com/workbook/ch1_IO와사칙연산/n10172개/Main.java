3b26b8580306ba7b91c874875fb08e66dfa1eb41
jinju <jinju6288@naver.com>
2021-07-25 16:17:12 +0900

    "를 출력하려면 역슬래시를 "앞에 붙인다

---
+++ Study_hotfive/src/com/workbook/ch1_IO와사칙연산/n10172개/Main.java
@@ -0,0 +1,21 @@
+package com.workbook.ch1_IO와사칙연산.n10172개;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+/*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+	}
+
+}
