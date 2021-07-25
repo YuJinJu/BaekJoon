93f9e5ac8802d8a91356e5bd8037054ed0c1ac74
jinju <jinju6288@naver.com>
2021-07-25 18:22:50 +0900

    BuffereReader, BufferedWriter

---
+++ Study_hotfive/src/com/workbook/ch3_for/n15552빠른더하기/Main.java
@@ -0,0 +1,25 @@
+package com.workbook.ch3_for.n15552빠른더하기;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int test_case = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<test_case;i++) {
+			String [] str = br.readLine().split(" ");
+			int a = Integer.parseInt(str[0]); 
+			int b = Integer.parseInt(str[1]); 
+			bw.write(a+b +"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
