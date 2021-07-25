daf0be649bfb4a85653183d0e5ee6bd2f5997866
jinju <jinju6288@naver.com>
2021-07-25 15:55:43 +0900

    success \를 출력하려면 두번 작성해야함 출력된 \는 한칸으로 처리된다

--- Study_hotfive/src/com/workbook/ch1_IO와사칙연산/n10171고양이/Main.java
+++ Study_hotfive/src/com/workbook/ch1_IO와사칙연산/n10171고양이/Main.java
@@ -3,16 +3,10 @@ package com.workbook.ch1_IO와사칙연산.n10171고양이;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(" \\    /\\");
+		System.out.println("\\    /\\");
 		System.out.println(" )  ( ')");
 		System.out.println("(  /  )");
 		System.out.println(" \\(__)|");
-/*
- \    /\
- )  ( ')
-(  /  )
- \(__)|
- */
-	}
 
+	}
 }
